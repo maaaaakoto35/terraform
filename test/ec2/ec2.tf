c933df2bc612d2eeba14950c6fe9cb9ab41f0a1e
maaaaakoto35 <hajimeyo13@icloud.com>
2020-06-18 23:23:28 +0900

    [develop] add definetely

---
+++ test/ec2/ec2.tf
@@ -0,0 +1,30 @@
+
+resource "aws_instance" "sandbox" {
+    ami = "ami-785c491f"
+    instance_type = "t2.micro"
+    # remote_state を指定している
+    subnet_id = "${data.terraform_remote_state.vpc.public_subnet_id}"
+}
+
+provider "aws" {
+    region = "ap-northeast-1"
+}
+
+# remote_state を設定し vpc という名前で参照できるようにしています
+data "terraform_remote_state" "vpc" {
+    backend = "s3"
+    config {
+        bucket = "unifood-dev"
+        key = "test/vpc/terraform.tfstate"
+        region = "ap-northeast-1"
+    }
+}
+
+terraform {
+    backend "s3" {
+        bucket = "unifood-dev"
+        # キー名は vpc のものとかぶらないようにします
+        key = "test/ec2/terraform.tfstate"
+        region = "ap-northeast-1"
+    }
+}
