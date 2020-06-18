a9a792669774c263bc5a26b73ca5463bdc659e19
maaaaakoto35 <hajimeyo13@icloud.com>
2020-06-18 22:37:19 +0900

    [develop] clean

--- ec2.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "sandbox" {
-  count         = 2              # インスタンス数
-  ami           = "ami-785c491f" # Ubuntu 16.04 LTS official ami
-  instance_type = "t2.micro"     # インスタンスタイプ
-
-  tags = {
-    Name = "${format("sandbox-%02d", count.index + 1)}"
-  }
-}
