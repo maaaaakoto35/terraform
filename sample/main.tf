9d9eb9f64c6a0b79a06d0d39918a4397f48ce944
maaaaakoto35 <hajimeyo13@icloud.com>
2020-07-16 00:30:39 +0900

    [develop] add aws-load-bracer

--- sample/main.tf
+++ sample/main.tf
@@ -1,4 +1,4 @@
-# main.tf
+############################################     main     ############################################
 # 2020/07/03
 
 module "web_server" {
@@ -6,22 +6,344 @@ module "web_server" {
     instance_type   = "t3.micro"
 }
 
-module "describe_regions_for_ec2" {
-    source      = "./iam_role"
-    name        = "describe-regions-for-ec2"
-    identifier  = "ec2.amazon.com"
-    policy      = data.aws_iam_policy_document.allow_describe_regions.json
-}
+# module "describe_regions_for_ec2" {
+#     source      = "./iam_role"
+#     name        = "describe-regions-for-ec2"
+#     identifier  = "ec2.amazon.com"
+#     policy      = data.aws_iam_policy_document.allow_describe_regions.json
+# }
 
-# ポリシードキュメントの定義
-data "aws_iam_policy_document" "allow_describe_regions" {
-    statement {
-        effect      = "Allow"
-        actions     = ["ec2:DescriveRegions"]
-        resources   = ["*"]
-    }
-}
+# # ポリシードキュメントの定義
+# data "aws_iam_policy_document" "allow_describe_regions" {
+#     statement {
+#         effect      = "Allow"
+#         actions     = ["ec2:DescriveRegions"]
+#         resources   = ["*"]
+#     }
+# }
 
 output "public_dns" {
     value = module.web_server.public_dns
 }
+
+
+
+############################################     s3     ############################################
+# 2020/07/15
+
+# プライベートバケットの定義
+resource "aws_s3_bucket" "private" {
+    bucket = "maaaaakoto-sample-terraform-private"
+
+    versioning {
+        enabled = true
+    }
+
+    server_side_encryption_configuration {
+        rule {
+            apply_server_side_encryption_by_default {
+                sse_algorithm = "AES256"
+            }
+        }
+    }
+}
+
+# ブロックパブリックアクセスの定義
+resource "aws_s3_bucket_public_access_block" "private" {
+    bucket                      = aws_s3_bucket.private.id
+    block_public_acls           = true
+    block_public_policy         = true
+    ignore_public_acls          = true
+    restrict_public_buckets     = true
+}
+
+# パブリックバケットの定義
+resource "aws_s3_bucket" "public" {
+    bucket  = "maaaaakoto-sample-terraform-public"
+    acl     = "public-read"
+
+    cors_rule {
+        allowed_origins = ["https://example.com"]
+        allowed_methods = ["GET"]
+        allowed_headers = ["*"]
+        max_age_seconds = 3000
+    }
+}
+
+# ALBからのログバケットの定義
+resource "aws_s3_bucket" "alb_log" {
+    bucket = "maaaaakoto-sample-terraform-alb-log"
+
+    lifecycle_rule {
+        enabled = true
+
+        expiration {
+            days = "180"
+        }
+    }
+}
+
+# ログバケッtのポリシーの定義
+resource "aws_s3_bucket_policy" "alb_log" {
+    bucket = aws_s3_bucket.alb_log.id
+    policy = data.aws_iam_policy_document.alb_log.json
+}
+
+# 59行目の中身
+data "aws_iam_policy_document" "alb_log" {
+    statement {
+        effect      = "Allow"
+        actions     = ["s3:PutObject"]
+        resources   = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
+
+        principals {
+            type = "AWS"
+            identifiers = ["582318560864"]
+        }
+    }
+}
+
+
+
+############################################     network     ############################################
+# 2020/07/15
+
+# VPCの定義
+resource "aws_vpc" "example" {
+    cidr_block              = "10.0.0.0/16"
+    enable_dns_support      = true
+    enable_dns_hostnames    = true
+
+    tags = {
+        Name = "example"
+    }
+}
+
+# インターネットゲートウェイの定義
+resource "aws_internet_gateway" "example" {
+    vpc_id = aws_vpc.example.id
+}
+
+
+############public_0############
+
+# パブリックサブネットの定義
+resource "aws_subnet" "public_0" {
+    vpc_id                      = aws_vpc.example.id        # 自動振り分け(多分)
+    cidr_block                  = "10.0.1.0/24"
+    map_public_ip_on_launch   = true
+    availability_zone           = "ap-northeast-1a"         # Tokyo
+}
+
+# パブリックルートテーブルの定義
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.example.id
+}
+
+# パブリックルートの定義
+resource "aws_route" "public" {
+    route_table_id          = aws_route_table.public.id
+    gateway_id              = aws_internet_gateway.example.id
+    destination_cidr_block  = "0.0.0.0/0"
+}
+
+# パブリックルートテーブルの関連付け
+resource "aws_route_table_association" "public_0" {
+    subnet_id       = aws_subnet.public_0.id
+    route_table_id  = aws_route_table.public.id
+}
+
+
+############private_0############
+
+# プライベートサブネットの定義
+resource "aws_subnet" "private_0" {
+    vpc_id                      = aws_vpc.example.id
+    cidr_block                  = "10.0.65.0/24"
+    map_public_ip_on_launch   = false                     # パブリックIPアドレスは不要
+    availability_zone           = "ap-northeast-1a"
+}
+
+# プライベートルートテーブルの定義
+resource "aws_route_table" "private_0" {
+    vpc_id = aws_vpc.example.id
+}
+
+# プライベートルートテーブルの関連付け
+resource "aws_route_table_association" "private_0" {
+    subnet_id       = aws_subnet.private_0.id
+    route_table_id  = aws_route_table.private_0.id
+}
+
+
+############NATgateway############
+
+# EIPの定義
+resource "aws_eip" "nat_gateway_0" {
+    vpc         = true
+    depends_on  = [aws_internet_gateway.example]
+}
+
+# NATゲートウェイの定義
+resource "aws_nat_gateway" "example_0" {
+    allocation_id   = aws_eip.nat_gateway_0.id
+    subnet_id       = aws_subnet.public_0.id
+    depends_on      = [aws_internet_gateway.example]
+}
+
+# プライベートルートのNATgatewayを利用したルートの定義 -> private_0->internet=true and private_0<-internet=false
+resource "aws_route" "private_0" {
+    route_table_id          = aws_route_table.private_0.id
+    nat_gateway_id          = aws_nat_gateway.example_0.id    # ここが39行目とは違う
+    destination_cidr_block  = "0.0.0.0/0"
+}
+
+
+############マルチAZ for 可用性############
+
+# パブリックサブネットの定義
+resource "aws_subnet" "public_1" {
+    vpc_id                      = aws_vpc.example.id        # 自動振り分け(共通)
+    cidr_block                  = "10.0.2.0/24"
+    map_public_ip_on_launch   = true
+    availability_zone           = "ap-northeast-1c"
+}
+
+# パブリックルートテーブルの関連付け
+resource "aws_route_table_association" "public_1" {
+    subnet_id       = aws_subnet.public_1.id
+    route_table_id  = aws_route_table.public.id             # ルートテーブル等は共有
+}
+
+# プライベートサブネットの定義
+resource "aws_subnet" "private_1" {
+    vpc_id                      = aws_vpc.example.id
+    cidr_block                  = "10.0.66.0/24"
+    map_public_ip_on_launch     = false                     # パブリックIPアドレスは不要
+    availability_zone           = "ap-northeast-1c"
+}
+
+# EIPの定義
+resource "aws_eip" "nat_gateway_1" {
+    vpc         = true
+    depends_on  = [aws_internet_gateway.example]
+}
+
+# NATゲートウェイの定義
+resource "aws_nat_gateway" "example_1" {
+    allocation_id   = aws_eip.nat_gateway_1.id
+    subnet_id       = aws_subnet.public_1.id
+    depends_on      = [aws_internet_gateway.example]
+}
+
+# プライベートルートテーブルの定義
+resource "aws_route_table" "private_1" {
+    vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route" "private_1" {
+    route_table_id          = aws_route_table.private_1.id
+    nat_gateway_id          = aws_nat_gateway.example_1.id    # ここが39行目とは違う
+    destination_cidr_block  = "0.0.0.0/0"
+}
+
+# プライベートルートテーブルの関連付け
+resource "aws_route_table_association" "private_1" {
+    subnet_id       = aws_subnet.private_1.id
+    route_table_id  = aws_route_table.private_1.id
+}
+
+
+############セキュリティグループ############
+
+# セキュリティグループのモジュールの呼び出し
+module "example_sg" {
+    source          = "./security_group"
+    name            = "module_sg"
+    vpc_id          = aws_vpc.example.id
+    port            = 80
+    cidr_blocks     = ["0.0.0.0/0"]
+}
+
+
+
+############################################     lb     ############################################
+# 2020/07/15
+
+# アプリケーションロードバランサーの定義
+resource "aws_lb" "example" {
+    name                        = "example"
+    load_balancer_type          = "application"      # この場合はALB networkの時はNLB
+    internal                    = false
+    idle_timeout                = 60
+    enable_deletion_protection  = false              # demo以外の時はここは true にしておく。(すぐに削除できないように。)
+
+    subnets = [
+        aws_subnet.public_0.id,
+        aws_subnet.public_1.id,
+    ]
+
+    access_logs {
+        bucket  = aws_s3_bucket.alb_log.id
+        enabled = true
+    }
+
+    security_groups = [
+        module.http_sg.security_group_id,
+        module.https_sg.security_group_id,
+        module.http_redirect_sg.security_group_id,
+    ]
+}
+
+output "alb_dns_name" {
+    value = aws_lb.example.arn
+}
+
+# ALBのセキュリティグループの定義 (HTTP)
+module "http_sg" {
+    source      = "./security_group"
+    name        = "http-sg"
+    vpc_id      = aws_vpc.example.id
+    port        = 80
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+# ALBのセキュリティグループの定義 (HTTPS)
+module "https_sg" {
+    source      = "./security_group"
+    name        = "https-sg"
+    vpc_id      = aws_vpc.example.id
+    port        = 443
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+# ALBのセキュリティグループの定義 (HTTPからHTTPSへリダイレクト)
+module "http_redirect_sg" {
+    source      = "./security_group"
+    name        = "http-redirect-sg"
+    vpc_id      = aws_vpc.example.id
+    port        = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+# HTTPリスナーの定義
+resource "aws_lb_listener" "http" {
+    load_balancer_arn   = aws_lb.example.arn
+    port                = "80"
+    protocol            = "HTTP"
+
+    default_action {
+        type = "fixed-response"
+
+        fixed_response {
+            content_type = "text/plain"
+            message_body = "これは HTTP です。"
+            status_code  = "200"
+        }
+    }
+}
+
+
+
+
+
